1c0dccf941ad197478e1b42357b59fb7fac6e503
juno-choi <ililil9482@naver.com>
2022-09-03 10:58:46 +0900

    project 시작

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  import Router from "svelte-spa-router";
+  import Home from "./Home.svelte";
+  import RoutineOrder from "./routine/RoutineOrder.svelte";
+  import RoutineSetting from "./routine/RoutineSetting.svelte";
+</script>
+
+<div class="py-12">
+  <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+    <div class="text-center">
+      <p
+        class="mt-2 text-3xl font-bold leading-8 tracking-tight text-blue-400 sm:text-4xl"
+      >
+        O.un
+      </p>
+    </div>
+  </div>
+</div>
+
+<Router
+  routes={{
+    "/": Home,
+    "/routine/order": RoutineOrder,
+    "/routine/setting": RoutineSetting,
+  }}
+/>
---
+++ src/Home.svelte
@@ -0,0 +1,25 @@
+<script>
+  function gettingReady() {
+    alert("해당 기능은 준비 중입니다...");
+  }
+</script>
+
+<div class="bg-white py-12">
+  <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+    <div class="text-center">
+      <a
+        href="/#/routine/order"
+        class="inline-block rounded-md border border-transparent bg-blue-500 py-3 px-8 text-center font-medium text-white hover:bg-blue-700"
+        >운동 시작하기</a
+      >
+    </div>
+    <div class="text-center mt-3">
+      <a
+        href="/#"
+        on:click={gettingReady}
+        class="inline-block rounded-md border border-transparent bg-blue-500 py-3 px-8 text-center font-medium text-white hover:bg-blue-700"
+        >루틴 불러오기</a
+      >
+    </div>
+  </div>
+</div>
